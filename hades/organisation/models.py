4b429f53f68e9239d959e1b64199341e4edddde4
Krishap-s <krishap.s.sreenivasan@gmail.com>
2021-11-02 19:48:29 +0530

    feat: add contact_no validation to organisation

--- hades/organisation/models.py
+++ hades/organisation/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from user.models import User
+
+def validate_phone_no(value):
+    if not value.isdigit() or len(value) != 10:
+       raise ValidationError("invalid phone number")
 
 class Organisation(models.Model):
     name = models.CharField(max_length=30)
     logo_pic = models.CharField(max_length=100)
     email = models.EmailField()
     website = models.CharField(max_length=100)
-    contact_no = models.CharField(max_length=10)
+    contact_no = models.CharField(max_length=10,validators=[validate_phone_no])
 
 class Member(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='member')
