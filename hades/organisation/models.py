d11ae8fe83552067a49c4f6ac52bd27809e2355d
Krishap-s <krishap.s.sreenivasan@gmail.com>
2021-11-14 15:11:10 +0530

    feat: add role system to organisation

--- hades/organisation/models.py
+++ hades/organisation/models.py
@@ -13,10 +13,20 @@ class Organisation(models.Model):
     website = models.CharField(max_length=100)
     contact_no = models.CharField(max_length=10,validators=[validate_phone_no])
 
+class Role(models.Model):
+    name = models.CharField(max_length=10)
+    organisation = models.ForeignKey(Organisation,on_delete=models.CASCADE,related_name='roles')
+    
+    class Meta:
+        unique_together = ('name','organisation')
+    
+    def __str__(self):
+        return self.name
+
 class Member(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='member')
     organisation = models.ForeignKey(Organisation,on_delete=models.CASCADE,related_name='member')
-    role = models.CharField(max_length=10)
+    role = models.ManyToManyField(Role,related_name='members',null=True)
 
     class Meta:
         unique_together = ('user','organisation')
--- hades/organisation/urls.py
+++ hades/organisation/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('create_link',OrganisationViewSet.as_view({'post':'create_link'})),
     path('join',OrganisationViewSet.as_view({'get':'join_organisation'})),
     path('assign_role',OrganisationViewSet.as_view({'post':'assign_role'})),
+    path('remove_role',OrganisationViewSet.as_view({'post':'deassign_role'})),
+    path('delete_member',OrganisationViewSet.as_view({'post':'delete_member'}))
 ]
